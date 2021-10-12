5b02866ba90cb0bcf0e1dae41987721b8b2eda99
dmitry <dmitry.sitnov@arcadia.spb.ru>
2021-10-12 19:18:41 +0300

    move fn that fetches transactions under api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,18 @@
 import currency from 'currency.js';
+import * as csv from 'fast-csv';
+import * as fs from 'fs';
 import * as path from 'path';
 
-export const TRANSACTION_API =  path.resolve(__dirname, '../data/transactions.csv');
+const TRANSACTION_API = path.resolve(__dirname, '../data/transactions.csv');
+const LATENCY_API = path.resolve(__dirname, '../data/api_latencies.json');
+
+interface TransactionCsvRow {
+  id: string;
+  amount: string;
+  bank_country_code: string;
+}
+
+type Latencies = { [key: string]: number };
 
 export interface Transaction {
   id: string;
@@ -10,10 +21,28 @@ export interface Transaction {
   bankName: string;
 }
 
-type Latencies = { [key: string]: number };
-const LATENCY_API = path.resolve(__dirname, '../data/api_latencies.json');
-const LATENCIES: Latencies = require(LATENCY_API);
+export function getTransactions(): Promise<Transaction[]> {
+  return new Promise((resolve, reject) => {
+    const transactions: Transaction[] = [];
+    
+    fs.createReadStream(TRANSACTION_API)
+      .pipe(csv.parse({ headers: true, }))
+      .on('data', (row: TransactionCsvRow) => {
+        transactions.push({
+          id: row.id,
+          bankCountryCode: row.bank_country_code,
+          bankName: `${row.bank_country_code} unknown`,
+          amount: currency(row.amount),
+        });
+      })
+      .on('error', error => reject(error))
+      .on('end', () => resolve(transactions));
+  });
+}
 
+
+// for the simplicity let's keep getLatency as a synchronous function
+const LATENCIES: Latencies = require(LATENCY_API);
 export function getLatency(transaction: Transaction): number {
   const key = transaction.bankCountryCode;
   if (!LATENCIES.hasOwnProperty(key)) {
@@ -22,3 +51,4 @@ export function getLatency(transaction: Transaction): number {
 
   return LATENCIES[key];
 }
+
